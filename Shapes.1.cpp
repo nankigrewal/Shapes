89d5adb1d5bbe0c215ffb9241811556df2289ad6
nankigrewal <nanki.k.grewal@gmail.com>
2018-09-19 10:50:06 -0700

    Added ternary operators for tokens length checking and dimensions setting, added a stream manipulator function, and made member functions instead of standard functions. Error to fix: given dimensions should not have 2 decimal points, only calculated values should.

--- Shapes.1.cpp
+++ Shapes.1.cpp
@@ -14,32 +14,40 @@ const double pi = 3.14159;
 
 struct Square {
 	double side;
+	void output(ostream&) const;
 };
 struct Rectangle {
 	double length;
 	double width;
+	void output(ostream&) const;
 };
 struct Circle {
 	double radius;
+	void output(ostream&) const;
 };
 struct Triangle {
 	double side;
+	void output(ostream&) const;
 };
 struct Cube {
 	double side;
+	void output(ostream&) const;
 };
 struct Box {
 	double length;
 	double width;
 	double height;
+	void output(ostream&) const;
 };
 struct Cylinder {
 	double radius;
 	double height;
+	void output(ostream&) const;
 };
 struct Prism {
 	double side;
 	double height;
+	void output(ostream&) const;
 };
 //Prototype for string parsing function
 vector<string> parseString(string);
@@ -52,22 +60,25 @@ void outputBox(ostream&, const Box&);
 void outputCube(ostream&, const Cube&);
 void outputCylinder(ostream&, const Cylinder&);
 void outputPrism(ostream&, const Prism&);
+//Stream manipulator prototype
+ostream& roundingTwo(ostream&);
 
 int main() {
-
-	//Programmer info
-	cout << "Programmer : Nanki Grewal\nProgrammer ID: 1428278" << endl;
-	cout << "File: " << __FILE__ << "\n\n";
 
 	//Declaring input and output file
 	ifstream fin;
 	ofstream fout;
-	//Declaring string to hold file input
-	string input;
-	
 	//Open the input and output files
 	fin.open("Shapes.input.txt");
 	fout.open("Shapes.output.txt");
+	//Declaring string to hold file input
+	string input;
+
+	//Programmer info
+	cout << "Programmer : Nanki Grewal\nProgrammer ID: 1428278" << endl;
+	cout << "File: " << __FILE__ << "\n\n";
+	fout << "Programmer : Nanki Grewal\nProgrammer ID: 1428278" << endl;
+	fout << "File: " << __FILE__ << "\n\n";
 
 	//Check if the file is usable/locatable
 	if (!fin.good()) throw "I/O error";
@@ -75,13 +86,7 @@ int main() {
 	//Create an empty bag and its companion array
 	vector<void*> myBag; 
 	vector<char> myBagType;
-
-	//Setting console and file output to two decimal points precision
-	cout.setf(ios::fixed);
-	cout.precision(2);
-	fout.setf(ios::fixed);
-	fout.precision(2);
-
+	
 	//While loop for parsing through the open file
 	while (fin.good()) {
 		//Read the line
@@ -98,67 +103,54 @@ int main() {
 		if (tokens.at(0) == "SQUARE") {
 			Square* sq = new Square;
 			//Side value needed
-			tokens.resize(2, "0");
+			sq->side = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
 			//Convert string to double
-			sq->side = atof(tokens.at(1).c_str());
 			myBag.push_back(sq);
 			myBagType.push_back('S');
 		}
 		else if (tokens.at(0) == "RECTANGLE") {
 			Rectangle* rec = new Rectangle;
-			//Length and width values needed
-			tokens.resize(3, "0");
 			//Convert strings to doubles
-			rec->length = atof(tokens.at(1).c_str());
-			rec->width = atof(tokens.at(2).c_str());
+			rec->length = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
+			rec->width = (tokens.size() > 2 ? atof(tokens.at(2).c_str()) : 0);
 			myBag.push_back(rec);
 			myBagType.push_back('R');
 		}
 		else if (tokens.at(0) == "TRIANGLE") {
 			Triangle* tri = new Triangle;
-			//Side value needed
-			tokens.resize(2, "0");
 			//Convert string to double
-			tri->side = atof(tokens.at(1).c_str());
+			tri->side = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
 			myBag.push_back(tri);
 			myBagType.push_back('T');
 		}
 		else if (tokens.at(0) == "CIRCLE") {
 			Circle* cir = new Circle;
-			// Radius value needed
-			tokens.resize(2, "0");
 			//Convert string to double
-			cir->radius = atof(tokens.at(1).c_str());
+			cir->radius = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
 			myBag.push_back(cir);
 			myBagType.push_back('C');
 		}
 		else if (tokens.at(0) == "CUBE") {
 			Cube* cube = new Cube;
-			//Side value needed
-			tokens.resize(2, "0");
 			//Convert string to double
-			cube->side = atof(tokens.at(1).c_str());
+			cube->side = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
 			myBag.push_back(cube);
 			myBagType.push_back('U');
 		}
 		else if (tokens.at(0) == "BOX") {
 			Box* box = new Box;
-			//Length, width, and height values needed
-			tokens.resize(4, "0");
 			//Convert strings to doubles
-			box->length = atof(tokens.at(1).c_str());
-			box->width = atof(tokens.at(2).c_str());
-			box->height = atof(tokens.at(3).c_str());
+			box->length = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
+			box->width = (tokens.size() > 2 ? atof(tokens.at(2).c_str()) : 0);
+			box->height = (tokens.size() > 3 ? atof(tokens.at(3).c_str()) : 0);
 			myBag.push_back(box);
 			myBagType.push_back('B');
 		}
 		else if (tokens.at(0) == "CYLINDER") {
 			Cylinder* cyl = new Cylinder;
-			//Radius and height values needed
-			tokens.resize(3, "0");
 			//Convert strings to doubles
-			cyl->height = atof(tokens.at(1).c_str());
-			cyl->radius = atof(tokens.at(2).c_str());
+			cyl->height = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
+			cyl->radius = (tokens.size() > 2 ? atof(tokens.at(2).c_str()) : 0);
 			myBag.push_back(cyl);
 			myBagType.push_back('Y');
 		}
@@ -167,8 +159,8 @@ int main() {
 			//Side and height values needed
 			tokens.resize(3, "0");
 			//Convert strings to doubles
-			pri->side = atof(tokens.at(1).c_str());
-			pri->height = atof(tokens.at(2).c_str());
+			pri->side = (tokens.size() > 1 ? atof(tokens.at(1).c_str()) : 0);
+			pri->height = (tokens.size() > 2 ? atof(tokens.at(2).c_str()) : 0);
 			myBag.push_back(pri);
 			myBagType.push_back('P');
 		}
@@ -187,50 +179,50 @@ int main() {
 		if (myBagType[i] == 'S') {
 			Square* pSquare = reinterpret_cast<Square*>(myBag[i]);
 			Square& rSquare = *pSquare;
-			outputSquare(cout, rSquare);
-			outputSquare(fout, rSquare);
+			rSquare.output(cout);
+			rSquare.output(fout);
 		}
 		else if (myBagType[i] == 'R') {
 			Rectangle* pRectangle = reinterpret_cast<Rectangle*>(myBag[i]);
 			Rectangle& rRectangle = *pRectangle;
-			outputRectangle(cout, rRectangle);
-			outputRectangle(fout, rRectangle);
+			rRectangle.output(cout);
+			rRectangle.output(fout);
 		}
 		else if (myBagType[i] == 'T') {
 			Triangle* pTriangle = reinterpret_cast<Triangle*>(myBag[i]); 
 			Triangle& rTriangle = *pTriangle;
-			outputTriangle(cout, rTriangle);
-			outputTriangle(fout, rTriangle);
+			rTriangle.output(cout);
+			rTriangle.output(fout);
 		}
 		else if (myBagType[i] == 'C') {
 			Circle* pCircle = reinterpret_cast<Circle*>(myBag[i]);
 			Circle& rCircle = *pCircle;
-			outputCircle(cout, rCircle);
-			outputCircle(fout, rCircle);
+			rCircle.output(cout);
+			rCircle.output(fout);
 		}
 		else if (myBagType[i] == 'U') {
 			Cube* pCube = reinterpret_cast<Cube*>(myBag[i]); 
 			Cube& rCube = *pCube;
-			outputCube(cout, rCube);
-			outputCube(fout, rCube);
+			rCube.output(cout);
+			rCube.output(fout);
 		}
 		else if (myBagType[i] == 'B') {
 			Box* pBox = reinterpret_cast<Box*>(myBag[i]); 
 			Box& rBox = *pBox;
-			outputBox(cout, rBox);
-			outputBox(fout, rBox);
+			rBox.output(cout);
+			rBox.output(fout);
 		}
 		else if (myBagType[i] == 'Y') {
 			Cylinder* pCylinder = reinterpret_cast<Cylinder*>(myBag[i]);
 			Cylinder& rCylinder = *pCylinder;
-			outputCylinder(cout, rCylinder);
-			outputCylinder(fout, rCylinder);
+			rCylinder.output(cout);
+			rCylinder.output(fout);
 		}
 		else if (myBagType[i] == 'P') {
 			Prism* pPrism = reinterpret_cast<Prism*>(myBag[i]);
 			Prism& rPrism = *pPrism;
-			outputPrism(cout, rPrism);
-			outputPrism(fout, rPrism);
+			rPrism.output(cout);
+			rPrism.output(fout);
 		}
 		else {
 			cout << "Type not recognized." << endl;
@@ -290,58 +282,92 @@ vector<string> parseString(string str) {
 	return vector<string>(begin, end);
 }
 
-void outputSquare(ostream& out, const Square& s) {
-	double area = s.side * s.side;
-	double perimeter = s.side * 4.0;
+void Square::output(ostream& out) const {
+	double area, perimeter;
+	double side;
+
+	side = this->side;
+	area = side * side;
+	perimeter = 4.0 * side;
 	//Output the data to the console and an output file
-	out << "Square side = " << s.side << ", area = " << area << ", perimeter = " << perimeter << endl;
+	out << roundingTwo << "Square side = " << side << ", area = " << area << ", perimeter = " << perimeter << endl;
 }
-void outputRectangle(ostream& out, const Rectangle& r) {
-	//Calculate area and perimeter using geometric formulas
-	double area = r.length * r.width;
-	double perimeter = 2.0 * (r.length + r.width);
+void Rectangle::output(ostream& out) const {
+	
+	double area, perimeter;
+	double length, width;
+
+	length = this->length;
+	width = this->width;
+	area = length * width;
+	perimeter = (2.0 * length) + (2.0 * width);
 	//Output the data to the console and an output file
-	out << "Rectangle length = " << r.length << ", width = " << r.width << ", area = " << area << ", perimeter = " << perimeter << endl;
+	out << roundingTwo << "Rectangle length = " << length << ", width = " << width << ", area = " << area << ", perimeter = " << perimeter << endl;
 }
-void outputCircle(ostream& out, const Circle& c) {
-	//Calculate area and perimeter using geometric formulas
-	double area = pi * c.radius * c.radius;
-	double perimeter = 2.0 * pi * c.radius;
+void Circle::output(ostream& out) const {
+	double area, perimeter;
+	double radius;
+	radius = this->radius;
+	area = pi * radius * radius;
+	perimeter = 2.0 * pi * radius;
 	//Output the data to the console and an output file
-	out << "Circle radius = " << c.radius << ", area = " << area << ", perimeter = " << perimeter << endl;
+	out << roundingTwo << "Circle radius = " << radius << ", area = " << area << ", perimeter = " << perimeter << endl;
 }
-void outputTriangle(ostream& out, const Triangle& t) {
-	//Calculate area and perimeter using geometric formulas
-	double area = sqrt(3) / 4.0 * t.side * t.side;
-	double perimeter = t.side * 3.0;
+void Triangle::output(ostream& out) const {
+	double area, perimeter;
+	double side;
+	side = this->side;
+	area = sqrt(3) / 4.0 * side * side;
+	perimeter = side * 3.0;
 	//Output the data to the console and an output file
-	out << "Triangle side = " << t.side << ", area = " << area << ", perimeter = " << perimeter << endl;
+	out << roundingTwo << "Triangle side = " << side << ", area = " << area << ", perimeter = " << perimeter << endl;
 }
-void outputBox(ostream& out, const Box& b) {
+void Box::output(ostream& out) const {
+	double surfaceArea, volume;
+	double length, width, height;
+	length = this->length;
+	width = this->width;
+	height = this->height;
+	surfaceArea = (2.0 * length * width) + (2.0 * width * height) + (2.0 * height * length);
+	volume = length * width * height;
+	//Output the data to the console and an output file
+	out << roundingTwo << "Box length = " << length << ", width = " << width << ", height = " << height << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
+}
+void Cube::output(ostream& out) const {
+	double surfaceArea, volume;
+	double side;
+	side = this->side;
 	//Calculate surface area and volume using geometric formulas
-	double surfaceArea = (2.0 * b.length * b.width) + (2.0 * b.width * b.height) + (2.0 * b.height * b.length);
-	double volume = b.length * b.width * b.height;
+	surfaceArea = 6.0 * side * side;
+	volume = side * side * side;
 	//Output the data to the console and an output file
-	out << "Box length = " << b.length << ", width = " << b.width << ", height = " << b.height << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
+	out << roundingTwo << "Cube side = " << side << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
 }
-void outputCube(ostream& out, const Cube& c) {
+void Cylinder::output(ostream& out) const {
+	double surfaceArea, volume;
+	double radius, height;
+	radius = this->radius;
+	height = this->height;
 	//Calculate surface area and volume using geometric formulas
-	double surfaceArea = 6.0 * c.side * c.side;
-	double volume = c.side * c.side * c.side;
+	surfaceArea = (2.0 * pi * radius * radius) + (2.0 * pi * radius * height);
+	volume = pi * radius * radius * height;
 	//Output the data to the console and an output file
-	out << "Cube side = " << c.side << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
+	out << roundingTwo << "Cylinder radius = " << radius << ", height = " << height << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
 }
-void outputCylinder(ostream& out, const Cylinder& c) {
+void Prism::output(ostream& out) const {
+	double surfaceArea, volume;
+	double side, height;
+	side = this->side;
+	height = this->height;
 	//Calculate surface area and volume using geometric formulas
-	double surfaceArea = (2.0 * pi * c.radius * c.radius) + (2.0 * pi * c.radius * c.height);
-	double volume = pi * c.radius * c.radius * c.height;
+	surfaceArea = (2.0 * sqrt(3) / 4.0 * side * side) + (3.0 * side * height);
+	volume = sqrt(3) / 4.0 * side * side * height;
 	//Output the data to the console and an output file
-	out << "Cylinder radius = " << c.radius << ", height = " << c.height << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
+	out << roundingTwo << "Prism side = " << side << ", height = " << height << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
 }
-void outputPrism(ostream& out, const Prism& p) {
-	//Calculate surface area and volume using geometric formulas
-	double surfaceArea = (2.0 * sqrt(3) / 4.0 * p.side * p.side) + (3.0 * p.side * p.height);
-	double volume = sqrt(3) / 4.0 * p.side * p.side * p.height;
-	//Output the data to the console and an output file
-	out << "Prism side = " << p.side << ", height = " << p.height << ", surface area = " << surfaceArea << ", volume = " << volume << endl;
+ostream& roundingTwo(ostream& out)
+{
+	out.setf(ios::fixed);
+	out.precision(2); // 2 decimal digits
+	return out;
 }
